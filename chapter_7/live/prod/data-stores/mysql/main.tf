7ebd84cb7bc7b1371873726ff9e665a3bdd966d3
Mateo Vega Castaño <matiusve09@outlook.com>
2025-09-17 16:13:40 -0500

    se agrega linea para la configuracion de un backend en caso opcional, se deja comentado

--- chapter_7/live/prod/data-stores/mysql/main.tf
+++ chapter_7/live/prod/data-stores/mysql/main.tf
@@ -6,6 +6,16 @@ terraform {
       version = ">=6.9.0"
     }
   }
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    # bucket         = "<YOUR S3 BUCKET>"
+    # key            = "<SOME PATH>/terraform.tfstate"
+    # region         = "us-east-2"
+    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
+    # encrypt        = true
+  }
 }
 
 provider "aws" {
