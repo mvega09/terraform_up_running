57e504c299db70e683ca0dabe483ae79f894073d
Mateo Vega Castaño <matiusve09@outlook.com>
2025-10-07 17:44:40 -0500

    Se agrega la ami ya que al momentod e validar el codigo estaba generando un error, esta linea se usa para realizar el depsliegue de una instancia EC2 mas recientes y que sea ubuntu 22.04 seerver jammy en el asg

--- chapter_8/small-modules/examples/asg/main.tf
+++ chapter_8/small-modules/examples/asg/main.tf
@@ -16,6 +16,7 @@ module "asg" {
   source = "../../modules/cluster/asg-rolling-deploy"
 
   cluster_name  = var.cluster_name
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   min_size           = 1
