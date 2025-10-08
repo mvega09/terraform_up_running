998a48522ab1d6d60abb8518a02e896083a1a825
Mateo Vega Castaño <matiusve09@outlook.com>
2025-10-08 16:03:22 -0500

    Se realiza modificacion a la linea 19 ya que se tenia una variable local la cual generaba error al desplegar el modulo alb, se agrega el archivo dependencies.tf para referenciar la subred creada en aws

--- chapter_9/examples/alb/main.tf
+++ chapter_9/examples/alb/main.tf
@@ -16,16 +16,5 @@ module "alb" {
   source = "../../modules/networking/alb"
 
   alb_name   = "${var.alb_name}-alb"
-  subnet_ids = local.subnet_ids
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+  subnet_ids = data.aws_subnets.default.ids
 }
